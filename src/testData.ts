2dd1f904cab27f325279b421a5da70ce3e18f26e
Luiz Gustavo <gugamatias7@gmail.com>
2021-02-17 16:17:16 -0300

    refactor: adding the id in the testData items

--- src/testData.ts
+++ src/testData.ts
@@ -1,21 +1,25 @@
 export const items = [
   {
+    id: 0,
     name: "Pikachu",
     types: ["eletricidade", "normal", "teste"],
     imageUrl: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/025.png",
   },
   {
+    id: 1,
     name: "Charmander",
     types: ["fogo", "dragão"],
     imageUrl: "https://cdn.bulbagarden.net/upload/7/73/004Charmander.png",
   },
   {
+    id: 2,
     name: "Squirtle",
     types: ["agua", "normal"],
     imageUrl:
       "https://cdn.bulbagarden.net/upload/thumb/3/39/007Squirtle.png/250px-007Squirtle.png",
   },
   {
+    id: 3,
     name: "Bulbasaur",
     types: ["terra", "normal"],
     imageUrl:

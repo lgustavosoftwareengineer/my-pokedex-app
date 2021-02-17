ace84f1988ac4b24149888e5b83e2ad56ca582c8
Luiz Gustavo <gugamatias7@gmail.com>
2021-02-17 16:15:42 -0300

    feat: implementing the search function in SearchPokemon screen

--- src/screens/SearchPokemon/index.tsx
+++ src/screens/SearchPokemon/index.tsx
@@ -1,10 +1,10 @@
 import { useNavigation } from "@react-navigation/native";
 import React, { useEffect, useState } from "react";
-import { ScrollView, View } from "react-native";
+import { View, Text } from "react-native";
 import Card from "../../components/Card";
 import { getUser } from "../../repositories/user.repository";
 
-import { Container, Input, Title } from "../../styles";
+import { Container, Input, Title, ScrollView } from "../../styles";
 import { items } from "../../testData";
 
 interface User {
@@ -13,9 +13,19 @@ interface User {
   sex?: number;
 }
 
+interface Pokemon {
+  id: number;
+  name: string;
+  types: string[];
+  imageUrl: string;
+}
+
 function SearchPokemon() {
   const [user, setUser] = useState<User>({});
   const [error, setError] = useState("");
+
+  const [searchValue, setSearchValue] = useState("");
+  const [filteredPokemons, setFilteredPokemons] = useState<Pokemon[]>([]);
 
   const { navigate } = useNavigation();
 
@@ -27,7 +37,7 @@ function SearchPokemon() {
         setError("Usuário não encontrado");
       }
     });
-  }, [user]);
+  }, [user, filteredPokemons]);
 
   function navigateToDetailsPage(id: number) {
     navigate("pokemon-details", {
@@ -35,20 +45,42 @@ function SearchPokemon() {
     });
   }
 
+  function handlerInput(event: string) {
+    setSearchValue(event);
+
+    if (event.length <= 1) {
+      setFilteredPokemons([]);
+    } else {
+      setFilteredPokemons(
+        items.filter((pokemon: Pokemon) => {
+          return pokemon.name.toLowerCase().includes(searchValue.toLowerCase());
+        })
+      );
+    }
+  }
+
   return (
     <Container>
-      <Input placeholder="Pesquise por um pokemon..."></Input>
+      <Input
+        placeholder="Pesquise por um pokemon..."
+        onChangeText={handlerInput}
+      ></Input>
+
       <View style={{ width: "100%", height: "100%" }}>
         <ScrollView showsVerticalScrollIndicator={false}>
-          {items.map((item, index) => (
-            <Card
-              key={index}
-              name={item.name}
-              types={item.types}
-              imageUrl={item.imageUrl}
-              onPress={() => navigateToDetailsPage(index)}
-            />
-          ))}
+          {filteredPokemons.length >= 1 ? (
+            filteredPokemons.map((item, index) => (
+              <Card
+                key={index}
+                name={item.name}
+                types={item.types}
+                imageUrl={item.imageUrl}
+                onPress={() => navigateToDetailsPage(item.id)}
+              />
+            ))
+          ) : (
+            <Title>Nenhum resultado para vc</Title>
+          )}
         </ScrollView>
       </View>
     </Container>

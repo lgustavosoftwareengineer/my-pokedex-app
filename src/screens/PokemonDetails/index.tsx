d714dc2908659d85e6820a8a21dacae2e84bd3dc
Luiz Gustavo <gugamatias7@gmail.com>
2021-02-17 16:16:15 -0300

    feat: improving the PokemonDetails

--- src/screens/PokemonDetails/index.tsx
+++ src/screens/PokemonDetails/index.tsx
@@ -1,16 +1,41 @@
 import { useNavigation, useRoute } from "@react-navigation/native";
 import React, { useEffect, useState } from "react";
 import { View, Text, Image } from "react-native";
+import { MaterialIcons as Icon } from "@expo/vector-icons/";
 
-import { Container, Title, Paragraph, SubTitle, Button } from "../../styles";
+import {
+  Container,
+  Title,
+  Paragraph,
+  Button as BackButton,
+} from "../../styles";
 
 import { items } from "../../testData";
 import { theme } from "../../theme";
+import {
+  Actions,
+  Button,
+  ButtonText,
+  Card,
+  Divisor,
+  PokemonName,
+  Skill,
+  SkillLabel,
+  SkillsContainer,
+  SectionTitle,
+  Header,
+  CardTitle,
+  CardSubtitle,
+} from "./style";
+import { ScrollView, TouchableOpacity } from "react-native-gesture-handler";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 interface Params {
   id: number;
 }
+
 interface Pokemon {
+  id?: number;
   name?: string;
   types?: string[];
   imageUrl?: string;
@@ -20,33 +45,109 @@ function PokemonDetails() {
   const route = useRoute();
   const [pokemon, setPokemon] = useState<Pokemon>({});
 
+  const [isOnMyPokedex, setIsOnMyPokedex] = useState({
+    text: "Adicionar na Pokedex",
+    state: false,
+  });
+  const [isOnMyFavorites, setIsOnMyFavorites] = useState({
+    text: "Adicionar aos favoritos",
+    state: false,
+  });
+
   const params = route.params as Params;
 
   const { navigate, goBack } = useNavigation();
 
+  const isOnMyFavoritesExpression = isOnMyFavorites.state
+    ? theme.colors.red
+    : theme.colors.black;
+
+  const isOnMyFavoritesPokedexExpression = isOnMyPokedex.state
+    ? theme.colors.red
+    : theme.colors.black;
+
+  function handlerAddInFavorites(id: number) {
+    setIsOnMyFavorites({
+      state: !isOnMyFavorites.state,
+      text: "Nos favoritos",
+    });
+  }
+  function handlerAddInPokedex(id: number) {
+    setIsOnMyPokedex({
+      state: !isOnMyPokedex.state,
+      text: "Na Pokedêx",
+    });
+  }
+
   useEffect(() => {
-    setPokemon(items[params.id]);
+    const pokemon = items.find((item) => item.id == params.id);
+    return setPokemon(pokemon ? pokemon : {});
   }, [pokemon]);
 
   return (
-    <Container>
-      <Title style={{ color: theme.colors.white }}>
-        <Title>Nome:</Title> {pokemon.name}
-      </Title>
-      <Image
-        source={{
-          uri: pokemon.imageUrl,
-        }}
-        style={{ width: 200, height: 200 }}
-      />
-      <SubTitle>Tipo</SubTitle>
-      {pokemon.types?.map((type, index) => (
-        <Paragraph key={index}>{type}</Paragraph>
-      ))}
-      <Button onPress={() => goBack()}>
-        <Paragraph>Voltar</Paragraph>
-      </Button>
-    </Container>
+    <SafeAreaView>
+      <ScrollView>
+        <Container>
+          <Header>
+            <TouchableOpacity onPress={() => goBack()}>
+              <Icon name="arrow-back" size={20} color={theme.colors.black} />
+            </TouchableOpacity>
+            <PokemonName>{pokemon.name}</PokemonName>
+            <View></View>
+          </Header>
+          <Image
+            source={{
+              uri: pokemon.imageUrl,
+            }}
+            style={{ width: 200, height: 200, margin: 20 }}
+          />
+
+          <Actions>
+            <Button onPress={() => handlerAddInPokedex(params.id)}>
+              <Icon
+                name="collections-bookmark"
+                size={20}
+                color={isOnMyFavoritesPokedexExpression}
+              />
+              <ButtonText color={isOnMyFavoritesPokedexExpression}>
+                {isOnMyPokedex.text}
+              </ButtonText>
+            </Button>
+            <Divisor />
+            <Button onPress={() => handlerAddInFavorites(params.id)}>
+              <Icon
+                name="favorite"
+                size={20}
+                color={isOnMyFavoritesExpression}
+              />
+              <ButtonText color={isOnMyFavoritesExpression}>
+                {isOnMyFavorites.text}
+              </ButtonText>
+            </Button>
+          </Actions>
+
+          <Card>
+            <CardTitle>Peso: 84kg</CardTitle>
+            <CardTitle>Status</CardTitle>
+            <CardSubtitle>Nome: {pokemon.name}</CardSubtitle>
+            <CardSubtitle>Valor: 100</CardSubtitle>
+          </Card>
+
+          <SectionTitle>Habilidades</SectionTitle>
+          <SkillsContainer
+            horizontal={true}
+            showsHorizontalScrollIndicator={false}
+          >
+            {pokemon.types?.map((type, index) => (
+              <Skill key={index}>
+                <SkillLabel>{type}</SkillLabel>
+                <SkillLabel>{type}</SkillLabel>
+              </Skill>
+            ))}
+          </SkillsContainer>
+        </Container>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
--- src/screens/PokemonDetails/style.ts
+++ src/screens/PokemonDetails/style.ts
@@ -1,2 +1,134 @@
 import styled from "styled-components/native";
 import { theme } from "../../theme";
+
+export const Header = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+
+  width: 100%;
+  padding: 15px;
+  margin-top: -15%;
+
+  color: #000;
+  background-color: #fff;
+
+  border-radius: 50px;
+`;
+
+export const PokemonName = styled.Text`
+  font-weight: 500;
+  font-size: 18px;
+  text-align: center;
+  line-height: 24px;
+
+  color: #000;
+  font-family: ${theme.fonts.title};
+`;
+
+export const Actions = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+
+  width: 100%;
+  padding: 15px;
+  margin-bottom: 20px;
+
+  color: #000;
+  background-color: #fff;
+  font-family: ${theme.fonts.normal};
+
+  border-radius: 50px;
+`;
+
+export const Button = styled.TouchableOpacity`
+  flex-direction: row;
+  width: 50%;
+  justify-content: space-between;
+  justify-content: center;
+`;
+
+interface ButtonTextProps {
+  color?: string;
+}
+export const ButtonText = styled.Text`
+  font-size: 12px;
+  font-family: ${theme.fonts.normal};
+  color: ${(props: ButtonTextProps) =>
+    props.color ? props.color : theme.colors.black};
+  padding-left: 5px;
+`;
+
+export const Divisor = styled.View`
+  width: 1px;
+  height: 100%;
+  background-color: ${theme.colors.black};
+  margin: 0 5px;
+`;
+
+export const Card = styled.View`
+  flex-direction: column;
+  margin-bottom: 20px;
+  width: 100%;
+  padding: 8%;
+  border-radius: 25px;
+
+  background-color: ${theme.colors.white};
+  color: ${theme.colors.black};
+`;
+
+export const CardTitle = styled.Text`
+  text-align: left;
+  font-family: ${theme.fonts.title};
+  font-size: 15px;
+  line-height: 18px;
+  color: ${theme.colors.black};
+`;
+
+export const CardSubtitle = styled.Text`
+  font-family: ${theme.fonts.subTitle};
+  font-size: 13px;
+  line-height: 18px;
+  margin-left: 10px;
+
+  color: ${theme.colors.black};
+`;
+
+export const SectionTitle = styled.Text`
+  text-align: left;
+  font-size: 20px;
+  line-height: 24px;
+  margin-bottom: 15px;
+  padding: 0px 10px;
+  width: 100%;
+
+  font-style: normal;
+  font-family: ${theme.fonts.subTitle};
+  color: ${theme.colors.black};
+`;
+
+export const SkillsContainer = styled.ScrollView`
+  width: 100%;
+  height: 150px;
+  padding: 5px;
+`;
+
+interface SkillProps {
+  color?: string;
+}
+export const Skill = styled.View`
+  background-color: ${(props: SkillProps) =>
+    props.color ? props.color : theme.colors.white};
+  border-radius: 25px;
+  padding: 5px;
+  min-width: 200px;
+  min-height: 50px;
+  margin-right: 8px;
+
+  justify-content: center;
+`;
+
+export const SkillLabel = styled.Text`
+  text-align: center;
+  color: ${theme.colors.black};
+  font-size: 12px;
+`;
